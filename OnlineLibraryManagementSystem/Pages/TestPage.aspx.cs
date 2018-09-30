88613dcd663e64c8ea87b9c23e1d9fcdafde969b
SHA <1026071147@qq.com>
2018-09-30 22:55:27 +0800

    基本能用了。

--- OnlineLibraryManagementSystem/Pages/TestPage.aspx.cs
+++ OnlineLibraryManagementSystem/Pages/TestPage.aspx.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using MySql.Data;
 
 public partial class Pages_TestPage : BasePage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            string OLMSDBConnectionString = ConfigurationManager.ConnectionStrings["OLMSDB"].ConnectionString;
+            MySqlConnection OLMSDBConnection = new MySqlConnection(OLMSDBConnectionString);
 
+            OLMSDBConnection.Open();
+            OLMSDBConnection.Close();
+        }
     }
 }
