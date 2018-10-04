c6f3ee2f98f5ab84cc29951a6952be18ee9b1b14
wangqi <1528272574@qq.com>
2018-10-04 21:12:18 +0800

    加入部分注册验证

--- OnlineLibraryManagementSystem/Pages/UserRegistration.aspx.cs
+++ OnlineLibraryManagementSystem/Pages/UserRegistration.aspx.cs
@@ -47,34 +47,49 @@ public partial class Pages_UserRegistration : BasePage
         {
             //后期补上正则匹配
             idNumber = TextBoxIDNumber.Text;
+            if(idNumber.Length != 18)
+            {
+                Response.Write("<script>window.alert('身份证号不合法!');</script>");
+                return;
+            }
             password = idNumber.Substring(idNumber.Length - 6);
         }
-        if (TextBoxTelephone.Text == "")
+        telephone = TextBoxTelephone.Text;
+        email = TextBoxEmail.Text;
+        if (! IsDeposit.Checked)
         {
-            Response.Write("<script>window.alert('电话号码不能为空!');</script>");
+            Response.Write("<script>window.alert('请缴纳押金!');</script>");
             return;
-        }
-        else
-        {
-            telephone = TextBoxTelephone.Text;
-        }
-        if (TextBoxEmail.Text == "")
-        {
-            Response.Write("<script>window.alert('邮箱不能为空!');</script>");
-            return;
-        }
-        else
-        {
-            email = TextBoxEmail.Text;
         }
         //链接数据库
         string OLMSDBConnectionString = ConfigurationManager.ConnectionStrings["OLMSDB"].ConnectionString;
         MySqlConnection OLMSDBConnection = new MySqlConnection(OLMSDBConnectionString);
+        //检测同账号是否注册过
+        string selectReaderSql = "select count(*) as num from Readers where Account = ?account or IdNumber = ?idNumber;";
         string insertReaderSql = "INSERT INTO Readers(Account, Password, Name, IdNumber, Phone, Email) " +
             "VALUES(?account, ?password, ?name, ?idNumber, ?telephone, ?email);";
         try
         {
             OLMSDBConnection.Open();
+            MySqlCommand cmd2 = new MySqlCommand(selectReaderSql, OLMSDBConnection);
+            cmd2.Parameters.AddWithValue("?account", account);
+            cmd2.Parameters.AddWithValue("?idNumber", idNumber);
+            MySqlDataReader reader = cmd2.ExecuteReader();
+            while (reader.Read())
+            {
+                if (reader.HasRows)
+                {
+                    Int64 count = (Int64)reader["num"];
+                    if(count > 0)
+                    {
+                        Response.Write("<script>window.alert('账号已存在 或 身份证号输入错误!');</script>");
+                        return;
+                    }
+                    break;
+                }
+            }
+            reader.Close();
+ 
             MySqlCommand cmd = new MySqlCommand(insertReaderSql, OLMSDBConnection);
             cmd.Parameters.AddWithValue("?account", account);
             cmd.Parameters.AddWithValue("?name", name);
