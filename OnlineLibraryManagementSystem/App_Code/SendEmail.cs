86ca6df9688a2129ba07b0bae5311e6bc022b1f9
NeutronStarT <31755002+NeutronStarT@users.noreply.github.com>
2018-10-13 13:54:19 +0800

    增加发送邮件功能，增加了一个定时器

---
+++ OnlineLibraryManagementSystem/App_Code/SendEmail.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+/// <summary>
+/// 发送邮件的相关操作
+/// </summary>
+public class SendEmail
+{
+    static System.Net.Mail.SmtpClient client = null;
+    public static void Send(string Receiver, string Subject, string content)
+    {
+        if (string.IsNullOrEmpty(Receiver) || string.IsNullOrEmpty(Subject)
+            || string.IsNullOrEmpty(content))
+        {
+            throw new ArgumentNullException("SendEmail参数空异常！");
+        }
+        if (client == null)
+        {
+            try
+            {
+                //163发送配置                    
+                client = new System.Net.Mail.SmtpClient();
+                client.Host = "smtp.163.com";
+                client.Port = 25;
+                client.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = true;
+
+
+                //qq发送配置的参数//切EnableSsl必须设置为true  
+                //client = new System.Net.Mail.SmtpClient();
+                //client.Host = "smtp.qq.com";
+                //client.Port = 25;
+                //client.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+                //client.EnableSsl = true;
+                //client.UseDefaultCredentials = false;
+
+                client.Credentials = new System.Net.NetworkCredential("spm_a4@163.com", "SPM12345");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        try
+        {
+            System.Net.Mail.MailMessage Message = new System.Net.Mail.MailMessage();
+            Message.SubjectEncoding = System.Text.Encoding.UTF8;
+            Message.BodyEncoding = System.Text.Encoding.UTF8;
+            Message.Priority = System.Net.Mail.MailPriority.High;
+
+            Message.From = new System.Net.Mail.MailAddress("spm_a4@163.com", "测试员");
+            //添加邮件接收人地址
+            string[] receivers = Receiver.Split(new char[] { ',' });
+            Array.ForEach(receivers.ToArray(), ToMail => { Message.To.Add(ToMail); });
+            //抄送自己一份
+            //Message.CC.Add("spm_a4@163.com");
+           
+            Message.Subject = Subject;
+            Message.Body = content;
+            Message.IsBodyHtml = true;
+            client.Send(Message);
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
+}
---
+++ OnlineLibraryManagementSystem/App_Code/TimerTasks.cs
@@ -0,0 +1,51 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// 定时器任务，主要包括定时短信提醒和清理过期预定
+/// </summary>
+public class TimerTasks
+{
+    public static void Task()
+    {
+        System.Timers.Timer timer = new System.Timers.Timer(10000)
+        {
+            //AutoReset 属性为 true 时，每隔指定时间循环一次
+            //如果为 false，则只执行一次
+            AutoReset = true,
+            Enabled = true
+        };
+        timer.Elapsed += new System.Timers.ElapsedEventHandler(Task_do);
+    }
+    static void Task_do(object sender, EventArgs e)
+    {
+        //暂时默认逾期时间为1天
+        TimeSpan overdue_time = TimeSpan.FromDays(1);
+        DateTime time_now = DateTime.Now;
+        string OLMSDBConnectionString = ConfigurationManager.ConnectionStrings["OLMSDB"].ConnectionString;
+        MySqlConnection conn = new MySqlConnection(OLMSDBConnectionString);
+        conn.Open();
+        try
+        {
+            string querySql = "SELECT RecordId, IssueTime FROM IssueRecords WHERE " +
+                            "ReturnTime is null ORDER BY IssueTime ASC";
+            MySqlCommand cmd = new MySqlCommand(querySql, conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                if (reader.HasRows)
+                {
+                    
+                }
+            }
+        }
+        finally
+        {
+            conn.Close();
+        }
+    }
+}
