50b6d98b58aaadf651af6bc799555f1a61abea7c
SHA <1026071147@qq.com>
2018-10-26 20:59:40 +0800

    修改了barcode图片生成bug

--- OnlineLibraryManagementSystem/Pages/LibrarianPages/SearchBooks.aspx.cs
+++ OnlineLibraryManagementSystem/Pages/LibrarianPages/SearchBooks.aspx.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Web.UI.WebControls;
 using MySql.Data.MySqlClient;
 using System.Configuration;
 using System.Data;
