514049698b2640bf4a727304603c72acaebf5433
Yutong Li <751654436@qq.com>
2018-11-08 18:02:20 +0800

    实现Admin修改密码

--- OnlineLibraryManagementSystem/Pages/AdminPages/ChangePassword.aspx.cs
+++ OnlineLibraryManagementSystem/Pages/AdminPages/ChangePassword.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -10,9 +11,30 @@ public partial class Pages_AdminPages_ChangePassword : BasePage
     protected void Page_Load(object sender, EventArgs e)
     {
         //检查登陆
-        if (string.IsNullOrEmpty((string)Session["aid"]))
+        if (string.IsNullOrEmpty((string)Session["lid"]))
         {
             Response.Write("<script type='text/javascript'>alert('" + Resources.Resource.LogInNotice + "');location.href='/Pages/AdminLogin.aspx';</script>");
         }
+    }
+
+    protected void btChangePassword_Click(object sender, EventArgs e)
+    {
+        //确保已经通过了所有验证控件
+        if (!rfvOldPassword.IsValid || !rfvNewPassword.IsValid || !rfvConfirmNewPassword.IsValid || !cvNewPassword.IsValid)
+            return;
+        string adminPassword = ConfigurationManager.AppSettings.Get("AdminPassword").ToString();
+        if (!tbOldPassword.Text.Equals(adminPassword))
+        {
+            Response.Write("<script>alert('" + Resources.Resource.WrongPassword + "')</script>");
+            return;
+        }
+        string newPassword = tbNewPassword.Text;
+        Configuration config = System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration(System.Web.HttpContext.Current.Request.ApplicationPath);
+        AppSettingsSection appSection = (AppSettingsSection)config.GetSection("appSettings");
+        appSection.Settings.Remove("AdminPassword");
+        appSection.Settings.Add("AdminPassword", newPassword);
+        config.Save();
+        Session["lid"] = null;
+        Response.Write("<script type='text/javascript'>alert('OK');location.href='../AdminLogin.aspx';</script>");
     }
 }
